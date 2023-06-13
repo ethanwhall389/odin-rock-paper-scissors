93ea4c5409616dc23dbc5b217333bb6c3f4cf59b
Ethan Hall <ethanwhall@icloud.com>
2023-06-12 21:57:12 -0400

    Work on adding ui to rps

--- script.js
+++ script.js
@@ -11,64 +11,78 @@ function gameRound (playerSelection, computerSelection) {
     let outputWin = `You win! ${playerSelection} beats ${computerSelection}`;
     let outputLose = `You lose! ${computerSelection} beats ${playerSelection}`;
     let outputTie = `It's a tie! You both chose ${playerSelection}`;
+    
 
     if ((lowerCaseComputer === 'rock' && lowerCasePlayer === 'paper') || (lowerCaseComputer === 'paper' && lowerCasePlayer === 'scissors') || (lowerCaseComputer === 'scissors' && lowerCasePlayer === 'rock') ) {
+        // numOfUserWins++;
         return outputWin;
     } else if ((lowerCaseComputer === 'rock' && lowerCasePlayer === 'scissors') || (lowerCaseComputer === 'paper' && lowerCasePlayer === 'rock') || (lowerCaseComputer === 'scissors' && lowerCasePlayer === 'paper') ) {
+        // numOfComputerWins++;
         return outputLose;
     } else {
+        // numOfTies++;
         return outputTie;
     }
 }
 
 function game () {
+    let currentRound = 1;
     let numOfUserWins = 0;
     let numOfComputerWins = 0;
     let numOfTies = 0;
-    let currentRound = 1;
-    for (let i = 0; i < 5; i++) {
-        
-        let bttnRock = document.querySelector('#rock');
-        let bttnPaper = document.querySelector('#paper');
-        let bttnScissors = document.querySelector('#scissors');
-        bttnRock.addEventListener('click', () => {
-            playerChoice = 'rock'
-            console.log(gameRound('rock', getComputerChoice()));
-        });
-        bttnPaper.addEventListener('click', () => {
-            playerChoice = 'paper'
-            console.log(gameRound('paper', getComputerChoice()));
-        });
-        bttnScissors.addEventListener('click', () => {
-            playerChoice = 'scissors'
-            console.log(gameRound('scissors', getComputerChoice()));
-        });
 
-        
-        if (winner.includes('lose')) {
-            numOfComputerWins++;
-        } else if (winner.includes('win')) {
-            numOfUserWins++;
-        } else {
-            numOfTies++;
-        }
+    let bttnRock = document.querySelector('#rock');
+    let bttnPaper = document.querySelector('#paper');
+    let bttnScissors = document.querySelector('#scissors');
+    let resultsDiv = document.querySelector('.resultsContainer');
+    let result = document.createElement('p');
+    resultsDiv.appendChild(result);
+    let numOfRounds = 5;
+    console.log(currentRound);
 
+
+
+    bttnRock.addEventListener('click', () => {
+        result.textContent = (gameRound('rock', getComputerChoice()));
         currentRound++;
-        console.log(`Current  round: ${currentRound} of 5.`)
+        console.log(`Current round: ${currentRound}`);
+        if (currentRound > numOfRounds) endGame();
+    });
 
-        console.log(`Number of computer wins so far: ${numOfComputerWins}.`);
-        console.log(`Number of user wins so far: ${numOfUserWins}.`);
-        console.log(`Number of ties so far: ${numOfTies}.`);
-    }
+    bttnPaper.addEventListener('click', () => {
+        result.textContent = (gameRound('paper', getComputerChoice()));
+        currentRound++;
+        console.log(currentRound);
+        if (currentRound > numOfRounds) endGame();
 
-    if (numOfUserWins > numOfComputerWins) {
-        alert(`You won the match! Your wins: ${numOfUserWins} | Computer wins: ${numOfComputerWins} | Ties: ${numOfTies} ties.`);
+    });
+    bttnScissors.addEventListener('click', () => {
+        result.textContent = (gameRound('scissors', getComputerChoice()));
+        currentRound++;
+        console.log(currentRound);
+        if (currentRound > numOfRounds) endGame();
+
+    });
+
+    // console.log(`Current  round: ${currentRound} of 5.`)
+
+    // console.log(`Number of computer wins so far: ${numOfComputerWins}.`);
+    // console.log(`Number of user wins so far: ${numOfUserWins}.`);
+    // console.log(`Number of ties so far: ${numOfTies}.`);
+
+
+
+
+    function endGame () {
+     if (numOfUserWins > numOfComputerWins) {
+        console.log(`You won the match! Your wins: ${numOfUserWins} | Computer wins: ${numOfComputerWins} | Ties: ${numOfTies} ties.`);
     } else if (numOfComputerWins > numOfUserWins) {
-        alert(`You lost the match! Your wins: ${numOfUserWins} | Computer wins: ${numOfComputerWins} | Ties: ${numOfTies} ties.`);
+       console.log(`You lost the match! Your wins: ${numOfUserWins} | Computer wins: ${numOfComputerWins} | Ties: ${numOfTies} ties.`);
     } else if (numOfUserWins === 0 && numOfComputerWins == 0) {
-        alert(`Nobody won the match! You had ${numOfUserWins} wins and the computer had ${numOfComputerWins}, but there were ${numOfTies} ties!`);
+        console.log(`Nobody won the match! You had ${numOfUserWins} wins and the computer had ${numOfComputerWins}, but there were ${numOfTies} ties!`);
+    }   
     }
-
+  
 }
 
 game();
